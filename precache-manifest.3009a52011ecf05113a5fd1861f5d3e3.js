9f387089f07d63607fbc1cc77e39fbe3c052d8c4
MatiSami <mateusz.samula@gmail.com>
2020-02-09 22:24:12 +0100

    Updates

---
+++ precache-manifest.3009a52011ecf05113a5fd1861f5d3e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51d74c2f464da4074dd699859aafa38",
+    "url": "/music-tabs/index.html"
+  },
+  {
+    "revision": "fcda7b064562f11140f9",
+    "url": "/music-tabs/static/css/main.82c74bd7.chunk.css"
+  },
+  {
+    "revision": "013554fb24cdb31530b5",
+    "url": "/music-tabs/static/js/2.1223adc3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/music-tabs/static/js/2.1223adc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcda7b064562f11140f9",
+    "url": "/music-tabs/static/js/main.956b3a65.chunk.js"
+  },
+  {
+    "revision": "b33ddf207adca1ec6451",
+    "url": "/music-tabs/static/js/runtime-main.1fee8e15.js"
+  },
+  {
+    "revision": "930129103e20cc1715052f52832eb067",
+    "url": "/music-tabs/static/media/bg.93012910.jpg"
+  }
+]);
